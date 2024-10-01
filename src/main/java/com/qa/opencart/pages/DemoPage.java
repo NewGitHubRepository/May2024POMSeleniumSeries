5d4c6fb4d52ae06a753ad6a950e088b2ccaf0499
NewGitHubRepository <geetanjalib0394@gmail.com>
2024-10-01 12:05:04 +0530

    added demopage

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class DemoPage {
+	
+  public static void main(String[] args) {
+	  int i = 10;
+	  int j = 10;
+	  int sum = i + j;
+	  System.out.println("Addition of two number: "+ sum);
+  }
+ 
+}
